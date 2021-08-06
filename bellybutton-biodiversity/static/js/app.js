b126459e665acb8bef5c3296197194e499135e38
DataTortuga <dcurtis390@gmail.com>
2021-08-06 15:18:46 -0500

    Fixed demopanl bugsworks properly now

--- bellybutton-biodiversity/static/js/app.js
+++ bellybutton-biodiversity/static/js/app.js
@@ -5,18 +5,23 @@ var select = d3.select('#selDataset')
 
 function update_panel(index, data) {
     var sample_panel = d3.select('#sample-metadata')
-    sample_panel.append('ul').append('li').text('ID: ' + data.metadata[index].id)
-    sample_panel.append('ul').append('li').text('Ethnicity: ' + data.metadata[index].ethnicity)
-    sample_panel.append('ul').append('li').text('Gender: ' + data.metadata[index].gender)
-    sample_panel.append('ul').append('li').text('Age: ' + data.metadata[index].age)
-    sample_panel.append('ul').append('li').text('Location: ' + data.metadata[index].location)
+    console.log(sample_panel.select('li'))
+    sample_panel.selectAll('li').remove()
+    // sample_panel.select('li').text('Ethnicity: ' + data.metadata[index].ethnicity)
+    // sample_panel.select('li').text('Gender: ' + data.metadata[index].gender)
+    // sample_panel.select('li').text('Age: ' + data.metadata[index].age)
+    // sample_panel.select('li').text('Location: ' + data.metadata[index].location)
+    sample_panel.append('ul').append('li').text('ID: ' + data.metadata[index].id);
+    sample_panel.append('ul').append('li').text('Ethnicity: ' + data.metadata[index].ethnicity);
+    sample_panel.append('ul').append('li').text('Gender: ' + data.metadata[index].gender);
+    sample_panel.append('ul').append('li').text('Age: ' + data.metadata[index].age);
+    sample_panel.append('ul').append('li').text('Location: ' + data.metadata[index].location);
 }
 
 function optionChanged(sam_id) {
     d3.json(data_loc).then(function (data) {
-        console.log(sam_id);
         for (let i = 0;  i < data.metadata.length; i++) {
-            if (data.metadata[i].id==sam_id) {update_panel(i, data); console.log(i)}
+            if (data.metadata[i].id==sam_id) {update_panel(i, data);}
         }
     })
 }
@@ -33,12 +38,6 @@ function init() {
         update_panel(0, data)
         
     })
-    trace = {
-        x: [1,2,3],
-        y: [1,2,3]
-    }
-    plot_data = [trace]
-    //Plotly.newPlot("plot", plot_data)
 }
 
 init()
