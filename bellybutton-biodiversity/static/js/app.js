9aca38dca20afad060848866cb2b73bbc8ee3e03
DataTortuga <dcurtis390@gmail.com>
2021-08-06 14:52:35 -0500

    Demopanel now updates when id is changed (buggy though)

--- bellybutton-biodiversity/static/js/app.js
+++ bellybutton-biodiversity/static/js/app.js
@@ -1,29 +1,37 @@
 const data_loc = '../samples.json';
 var select = d3.select('#selDataset')
-var sample_panel = d3.select('#sample-metadata')
-console.log(sample_panel)
+
+
+
+function update_panel(index, data) {
+    var sample_panel = d3.select('#sample-metadata')
+    sample_panel.append('ul').append('li').text('ID: ' + data.metadata[index].id)
+    sample_panel.append('ul').append('li').text('Ethnicity: ' + data.metadata[index].ethnicity)
+    sample_panel.append('ul').append('li').text('Gender: ' + data.metadata[index].gender)
+    sample_panel.append('ul').append('li').text('Age: ' + data.metadata[index].age)
+    sample_panel.append('ul').append('li').text('Location: ' + data.metadata[index].location)
+}
+
+function optionChanged(sam_id) {
+    d3.json(data_loc).then(function (data) {
+        console.log(sam_id);
+        for (let i = 0;  i < data.metadata.length; i++) {
+            if (data.metadata[i].id==sam_id) {update_panel(i, data); console.log(i)}
+        }
+    })
+}
 
 function init() {
 
     d3.json(data_loc).then(function (response) {
         var data = response
-        console.log(data)
 
         for(index in data.samples) {
-            //console.log(data.samples[index].id)
             select.append('option').text(data.samples[index].id)
         }
 
-        sample_panel.append('ul').append('li').text('ID: ' + data.metadata[0].id)
-        sample_panel.append('ul').append('li').text('Ethnicity: ' + data.metadata[0].ethnicity)
-        sample_panel.append('ul').append('li').text('Gender: ' + data.metadata[0].gender)
-        sample_panel.append('ul').append('li').text('Age: ' + data.metadata[0].age)
-        sample_panel.append('ul').append('li').text('Location: ' + data.metadata[0].location)
-
-
-
-
-
+        update_panel(0, data)
+        
     })
     trace = {
         x: [1,2,3],
