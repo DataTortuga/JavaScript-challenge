3743fb471592cd5675a4de808589267083117a14
DataTortuga <dcurtis390@gmail.com>
2021-08-06 15:22:17 -0500

    Added some comments

--- bellybutton-biodiversity/static/js/app.js
+++ bellybutton-biodiversity/static/js/app.js
@@ -1,16 +1,11 @@
 const data_loc = '../samples.json';
 var select = d3.select('#selDataset')
 
-
-
+// Function updates Demographic info panel
 function update_panel(index, data) {
     var sample_panel = d3.select('#sample-metadata')
     console.log(sample_panel.select('li'))
     sample_panel.selectAll('li').remove()
-    // sample_panel.select('li').text('Ethnicity: ' + data.metadata[index].ethnicity)
-    // sample_panel.select('li').text('Gender: ' + data.metadata[index].gender)
-    // sample_panel.select('li').text('Age: ' + data.metadata[index].age)
-    // sample_panel.select('li').text('Location: ' + data.metadata[index].location)
     sample_panel.append('ul').append('li').text('ID: ' + data.metadata[index].id);
     sample_panel.append('ul').append('li').text('Ethnicity: ' + data.metadata[index].ethnicity);
     sample_panel.append('ul').append('li').text('Gender: ' + data.metadata[index].gender);
@@ -18,6 +13,7 @@ function update_panel(index, data) {
     sample_panel.append('ul').append('li').text('Location: ' + data.metadata[index].location);
 }
 
+// Defines what to do when subject ID is changed
 function optionChanged(sam_id) {
     d3.json(data_loc).then(function (data) {
         for (let i = 0;  i < data.metadata.length; i++) {
@@ -26,17 +22,13 @@ function optionChanged(sam_id) {
     })
 }
 
+// Initializes webpage with data
 function init() {
-
-    d3.json(data_loc).then(function (response) {
-        var data = response
-
+    d3.json(data_loc).then(function (data) {
         for(index in data.samples) {
             select.append('option').text(data.samples[index].id)
         }
-
         update_panel(0, data)
-        
     })
 }
 
